2d2ffea676d16fcf1c83dc9d8ce21106ce7869f0
unknown <anabelens16@gmail.com>
2019-10-11 11:36:30 +0200

    script

--- script/deliver.bat
+++ script/deliver.bat
@@ -1,4 +1,4 @@
 @echo OFF
 set ARTIFACT=superPoker.war
-set DESTDIR=C:\Users\User\Documents\Formation DevOps\server\apache-tomcat-9.0.24\webapps
+set DESTDIR=C:\Users\User\Documents\Formation DevOps\server\tomcat\webapps
 copy target\%ARTIFACT% %DESTDIR%
