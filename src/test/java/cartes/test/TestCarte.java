8c3e75b93bc89db85c67363c6fb928f5d5ddd898
unknown <anabelens16@gmail.com>
2019-10-11 09:58:15 +0200

    add src web

--- src/test/java/cartes/test/TestCarte.java
+++ src/test/java/cartes/test/TestCarte.java
@@ -13,45 +13,38 @@ class TestCarte {
 
 	@Test
 	void testConstructeur() {
-		//given
-		
+		// given
 		Signe signe = Signe.CARREAU;
 		Valeur valeur = Valeur.DAME;
-		
-		//when
-		
-		Carte carte = new Carte( valeur, signe);
-		
-		//then
+		// when
+		Carte carte = new Carte(valeur, signe);
+		// then
 		assertAll(
-		()->assertEquals(signe, carte.getSigne()),
-		()->assertEquals(valeur, carte.getValeur()),
-		()->assertEquals(Couleur.ROUGE, carte.getCouleur())
-		
+			()->assertEquals(signe, carte.getSigne()),
+			()->assertEquals(valeur, carte.getValeur()),
+			()->assertEquals(Couleur.ROUGE, carte.getCouleur())
 		);
-		
 	}
 	
-		
 	@Test
-    void testEqualsNull() {
-        // given
-        Carte c = new Carte(Valeur.DAME, Signe.COEUR);
-        // when
-        boolean res = c.equals(null);
-        // then
-        assertFalse(res);
-    }
-
-    @Test
-    void testEqualsFieldsEquals() {
-        // given
-        Carte c1 = new Carte(Valeur.DAME, Signe.COEUR);
-        Carte c2 = new Carte(Valeur.DAME, Signe.COEUR);
-        // when
-        boolean res = c1.equals(c2);
-        // then
-        assertTrue(res);
-    }
+	void testEqualsNull() {
+		// given
+		Carte c = new Carte(Valeur.DAME, Signe.COEUR);
+		// when
+		boolean res = c.equals(null);
+		// then
+		assertFalse(res);
+	}
+	
+	@Test
+	void testEqualsFieldsEquals() {
+		// given
+		Carte c1 = new Carte(Valeur.DAME, Signe.COEUR);
+		Carte c2 = new Carte(Valeur.DAME, Signe.COEUR);
+		// when
+		boolean res = c1.equals(c2);
+		// then
+		assertTrue(res);
+	}
 
 }
