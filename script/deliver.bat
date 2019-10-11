874eb401c72a0b09f0b244cc3692302ce95628b2
unknown <anabelens16@gmail.com>
2019-10-11 11:17:30 +0200

    add script

--- script/deliver.bat
+++ script/deliver.bat
@@ -1,4 +1,4 @@
 @echo OFF
-set ARTIFACT=superPoker.jar
-set DESTDIR=C:\MyApps
+set ARTIFACT=superPoker.war
+set DESTDIR=C:\Users\User\Documents\Formation DevOps\server\apache-tomcat-9.0.24\webapps
 copy target\%ARTIFACT% %DESTDIR%
